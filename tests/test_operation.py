980b938fa5dc33e156a1efe5aeae0b1a43f6b928
Sahulinkan7 <sahulinkan7@gmail.com>
2024-09-18 19:19:23 +0530

    updated test file

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,20 +1,19 @@
 from src.maths_operation import math_add,math_div,math_sub,math_mul
-
+import pytest
 
 def test_math_add():
     assert math_add(2,3)==5
-    assert math_add(3,3)==6
-    
+        
 def test_math_mul():
     assert math_mul(4,5)==20
-    assert math_mul(5,7)==35
     
 def test_math_sub():
     assert math_sub(4,5)==-1
-    assert math_sub(99,90)==9
     
 def test_math_div():
     assert math_div(5,5)==1.0
-    assert math_div(10,2)==5.0
-    assert math_div(18,3)==6.0
+    
+def test_math_div_2():
+    with pytest.raises(ZeroDivisionError):
+        math_div(55,0)
     
