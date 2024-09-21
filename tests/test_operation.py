c436b1aefe470a8d333ff837d16bc034ab114f09
Sahulinkan7 <sahulinkan7@gmail.com>
2024-09-21 16:47:27 +0530

    pytest fixture test

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,19 +1,25 @@
 from src.maths_operation import math_add,math_div,math_sub,math_mul
 import pytest
 
-def test_math_add():
+@pytest.fixture
+def fixturetest():
+    print("pre test using fixture")
+    yield
+    print("post test using fixture")
+
+def test_math_add(fixturetest):
     assert math_add(2,3)==5
         
-def test_math_mul():
+def test_math_mul(fixturetest):
     assert math_mul(4,5)==20
     
-def test_math_sub():
+def test_math_sub(fixturetest):
     assert math_sub(4,5)==-1
     
-def test_math_div():
+def test_math_div(fixturetest):
     assert math_div(5,5)==1.0
     
-def test_math_div_2():
+def test_math_div_2(fixturetest):
     with pytest.raises(ZeroDivisionError):
         math_div(55,0)
     
